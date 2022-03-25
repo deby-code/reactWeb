707e6362b8185bb6fda77783ba7d420d4f919594
deby-code <deborahserranoqba@gmail.com>
2022-03-25 13:35:16 -0400

    List clients

--- src/components/ClientListComponent.js
+++ src/components/ClientListComponent.js
@@ -4,28 +4,43 @@ class ClientListComponent extends React.Component {
     state = {
         clients: []
     };
+
     componentDidMount() {
         const apiUrl = 'https://localhost:7161/api/clients';
         fetch(apiUrl)
             .then((response) => response.json())
             .then((data) => this.setState({clients: data}));
     }
+
     render() {
         return <ul>
-            <h2 className='list-head'>Available Public Repositories</h2>
-            {
-                this.state.clients.map(client =>(
-                        <div>
-                            {client.id}
-                        </div>
-                    )
-                )
+            <h2 className='list-head'>Clients List</h2>
 
-            }
+            <table className="table container table-striped">
+                <thead>
+                <tr>
+                    <th scope="col">#</th>
+                    <th scope="col">First</th>
+                    <th scope="col">Last</th>
+                    <th scope="col">Is overweight</th>
+                </tr>
+                </thead>
+                <tbody>
+                {
+                    this.state.clients.map(client => (
+                        <tr>
+                            <th scope="row"> {client.id}</th>
+                            <td> {client.name}</td>
+                            <td> {client.lastName}</td>
+                            <td>{client.isOverWeight ? 'Si' : 'No'}</td>
+                        </tr>))
 
-
+                }
+                </tbody>
+            </table>
 
         </ul>;
     }
 }
+
 export default ClientListComponent;
